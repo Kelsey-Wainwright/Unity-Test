7ef4c2c3168480b899fa3b7e2fbf8056e3d15bea
Kelsey-Wainwright <kelseywainwright09@gmail.com>
2024-01-12 07:31:31 +0100

    Initial Commit

---
+++ First Project/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Reference to Rigidbody Component: "rb"
+    public Rigidbody rb;
+
+    // Creates Variable for Forward Movement of Player, that Can Be Accessed From Unity
+    public float forwardForce = 200f;
+    // Creates Variable for Sideways Movement of Player, that Can Be Accessed From Unity
+    public float sidewaysForce = 300f;
+
+    // Things that Iterate Every Frame (Fixed is Used for Physics)
+    void FixedUpdate() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
